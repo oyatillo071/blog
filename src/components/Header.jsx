a56147d7619a093b6b9451c39870fd480b56c189
oyatillo071 <oyatillotoshtemirov071@gmail.com>
2025-01-11 02:11:45 +0500

    refactor  header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,56 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ThemeProvider from "./ThemeProvider";
+
+import NavBar from "../pages/NavBar";
 import { NavLink } from "react-router-dom";
-import { ExitIcon } from "@radix-ui/react-icons";
 
 function Header() {
-  const [isLogin, setIsLogin] = useState(false);
-
-  useEffect(() => {
-    const loginStatus = localStorage.getItem("user") ? true : false;
-    setIsLogin(loginStatus);
-  }, []);
-
   return (
-    <header className="flex items-center sticky flex-wrap-reverse sm:flex-nowrap gap-3 top-0 z-50 bg-white justify-between py-4 px-6  w-full dark:bg-gray-800 shadow-md">
-      <nav className="flex gap-6 container mx-auto flex-wrap-reverse">
-        <NavLink
-          to="/"
-          className={` dark:text-gray-300 font-mono hover:text-indigo-500 `}
-        >
-          Articles
-        </NavLink>
-        {isLogin ? (
-          <div className="flex items-center sm:gap-20 gap-5 flex-wrap-reverse">
-            <NavLink
-              to="/articles/create"
-              className={` dark:text-gray-300 font-mono hover:text-indigo-500`}
-            >
-              Create Article
-            </NavLink>
-            <NavLink
-              to="/login"
-              onClick={() => {
-                localStorage.setItem("user");
-              }}
-              className={`flex items-center gap-2 dark:text-gray-300 font-mono hover:text-indigo-500 
-              `}
-            >
-              <ExitIcon />
-              Exit
-            </NavLink>
-          </div>
-        ) : (
-          <NavLink
-            to="/login"
-            className={` dark:text-gray-300 font-mono hover:text-indigo-500 
-              `}
-          >
-            Login
-          </NavLink>
-        )}
-      </nav>
+    <header className="flex items-center static sm:sticky flex-wrap-reverse sm:flex-nowrap gap-3 top-0 z-50 bg-white justify-between py-4 px-6  w-full dark:bg-gray-800 shadow-md">
+      <NavBar />
       <ThemeProvider />
+      <NavLink
+        to="/articles/create"
+        className={` dark:text-gray-300 font-mono hover:text-indigo-500 sm:hidden block`}
+      >
+        <span>Create</span>
+      </NavLink>
     </header>
   );
 }
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { ExitIcon } from "@radix-ui/react-icons";
+import { useEffect } from "react";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const loginStatus = localStorage.getItem("user") ? true : false;
+    setIsLogin(loginStatus);
+  }, []);
+  return (
+    <nav className="flex gap-6 container mx-auto ">
+      <NavLink
+        to="/"
+        className={` dark:text-gray-300 font-mono hover:text-indigo-500 `}
+      >
+        Articles
+      </NavLink>
+      {isLogin ? (
+        <div className="flex items-center justify-between max-w-[80%] w-full sm:w-30% md:w-full sm:gap-20 gap-5 ">
+          <NavLink
+            to="/articles/create"
+            className={` dark:text-gray-300 font-mono hover:text-indigo-500`}
+          >
+            <span className="hidden sm:block">Create Article</span>
+          </NavLink>
+          <NavLink
+            to="/login"
+            onClick={() => {
+              localStorage.setItem("user");
+            }}
+            className={`flex items-center gap-2 dark:text-gray-300 font-mono hover:text-indigo-500 
+              `}
+          >
+            <ExitIcon />
+            <span className="hidden sm:block">Quit</span>
+          </NavLink>
+        </div>
+      ) : (
+        <NavLink
+          to="/login"
+          className={` dark:text-gray-300 font-mono hover:text-indigo-500 
+              `}
+        >
+          Login
+        </NavLink>
+      )}
+    </nav>
+  );
 }
 
 export default NavBar;
